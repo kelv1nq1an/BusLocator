c65cbef8f0b081b9d3f5640f843dc98e1e6b66db
kelvin <qxm329232741@gmail.com>
2016-06-28 17:31:28 +0800

    initial

---
+++ app/src/main/java/me/fattycat/kun/bustimer/BasePresenter.java
@@ -0,0 +1,14 @@
+package me.fattycat.kun.bustimer;
+
+/**
+ * Author: Kelvinkun
+ * Date: 16/6/28
+ */
+
+public interface BasePresenter {
+
+    void subscribe();
+
+    void unsubscribe();
+
+}
---
+++ app/src/main/java/me/fattycat/kun/bustimer/main/MainActivity.java
@@ -0,0 +1,11 @@
+package me.fattycat.kun.bustimer.main;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Author: Kelvinkun
+ * Date: 16/6/28
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
