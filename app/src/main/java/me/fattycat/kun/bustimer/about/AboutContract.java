9c6f96acb933860e40a5655d5b1731e8913f299d
kelvin <qxm329232741@gmail.com>
2016-07-05 10:40:45 +0800

    About page.

---
+++ app/src/main/java/me/fattycat/kun/bustimer/about/AboutContract.java
@@ -0,0 +1,19 @@
+package me.fattycat.kun.bustimer.about;
+
+import me.fattycat.kun.bustimer.BasePresenter;
+import me.fattycat.kun.bustimer.BaseView;
+
+/**
+ * Author: Kelvinkun
+ * Date: 16/6/30
+ */
+
+public interface AboutContract {
+    interface AboutView extends BaseView<AboutPresenter> {
+
+    }
+
+    interface AboutPresenter extends BasePresenter {
+
+    }
+}
---
+++ app/src/main/java/me/fattycat/kun/bustimer/about/AboutFragment.java
@@ -0,0 +1,32 @@
+package me.fattycat.kun.bustimer.about;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.fattycat.kun.bustimer.R;
+
+/**
+ * Author: Kelvinkun
+ * Date: 16/7/1
+ */
+
+public class AboutFragment extends Fragment implements AboutContract.AboutView {
+
+    private AboutContract.AboutPresenter aboutPresenter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.layout_fragment_about, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void setPresenter(AboutContract.AboutPresenter presenter) {
+        this.aboutPresenter = presenter;
+    }
+}
