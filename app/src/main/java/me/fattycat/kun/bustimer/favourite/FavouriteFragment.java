557700b755bcb7c645a3ef5091a734625bc92d78
kelvin <qxm329232741@gmail.com>
2016-07-22 10:04:15 +0800

    Load saved lines in favorite page.

--- app/src/main/java/me/fattycat/kun/bustimer/favourite/FavouriteFragment.java
+++ app/src/main/java/me/fattycat/kun/bustimer/favourite/FavouriteFragment.java
@@ -8,10 +8,18 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import me.fattycat.kun.bustimer.R;
+import me.fattycat.kun.bustimer.common.LineListAdapter;
+import me.fattycat.kun.bustimer.model.LineEntity;
+import me.fattycat.kun.bustimer.model.LineEntityWrapper;
+import me.fattycat.kun.bustimer.model.LineInfoSerializable;
+import me.fattycat.kun.bustimer.util.FavoriteDataUtil;
 
 /**
  * Author: Kelvinkun
@@ -32,15 +40,20 @@ public class FavouriteFragment extends android.support.v4.app.Fragment implement
     public android.view.View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         android.view.View root = inflater.inflate(R.layout.layout_fragment_favourite, container, false);
         unbinder = ButterKnife.bind(this, root);
-        FavouritePresenter favouritePresenter = new FavouritePresenter(this);
 
         favouriteRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        favouriteContractPresenter.loadLocalData();
 
         lineListAdapter = new LineListAdapter(getActivity(), null);
         favouriteRecyclerView.setAdapter(lineListAdapter);
 
+        new FavouritePresenter(this);
         return root;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        favouriteContractPresenter.loadLocalData();
     }
 
     @Override
@@ -56,6 +69,25 @@ public class FavouriteFragment extends android.support.v4.app.Fragment implement
 
     @Override
     public void onDataLoaded() {
-        lineListAdapter.setData(null);
+        List<LineInfoSerializable> allFavoriteLines = FavoriteDataUtil.loadAllFavoriteLine(getActivity());
+        List<LineEntityWrapper> allFavoriteLineWrappers = new ArrayList<>();
+        for (LineInfoSerializable line : allFavoriteLines) {
+            LineEntity.ResultEntity resultEntity = new LineEntity.ResultEntity();
+            resultEntity.setEndTime1(line.getEndTime1());
+            resultEntity.setStartTime(line.getStartTime());
+            resultEntity.setStartStation(line.getStartStation());
+            resultEntity.setRunFlag(line.getRunFlag());
+            resultEntity.setRunPathName(line.getRunPathName());
+            resultEntity.setBusInterval(line.getBusInterval());
+            resultEntity.setEndStation(line.getEndStation());
+            resultEntity.setRunPathId(line.getRunPathId());
+            resultEntity.setEndTime(line.getEndTime());
+            resultEntity.setStartTime1(line.getStartTime1());
+            LineEntity lineEntity = new LineEntity();
+            lineEntity.setResult(resultEntity);
+            LineEntityWrapper lineEntityWrapper = new LineEntityWrapper(lineEntity, line.getFlag());
+            allFavoriteLineWrappers.add(lineEntityWrapper);
+        }
+        lineListAdapter.setData(allFavoriteLineWrappers);
     }
 }
