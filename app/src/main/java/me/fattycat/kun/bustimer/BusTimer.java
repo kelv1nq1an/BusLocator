8959f29207ffe2f56a44cf50660428339e3b6b0b
fattycat <fattycat.u@gmail.com>
2016-11-25 14:03:47 +0800

    Update gradle

--- app/src/main/java/me/fattycat/kun/bustimer/BusTimer.java
+++ app/src/main/java/me/fattycat/kun/bustimer/BusTimer.java
@@ -1,8 +1,6 @@
 package me.fattycat.kun.bustimer;
 
 import android.app.Application;
-import com.umeng.analytics.MobclickAgent;
-import im.fir.sdk.FIR;
 
 /**
  * Author: Kelvinkun
@@ -14,9 +12,5 @@ public class BusTimer extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        FIR.init(this, AppSecret.BUGHD_APP_KEY);
-        MobclickAgent.startWithConfigure(new MobclickAgent.UMAnalyticsConfig(this, AppSecret.UEMNG_APP_KEY, "fir"));
-        MobclickAgent.openActivityDurationTrack(false);
-        MobclickAgent.setCatchUncaughtExceptions(false);
     }
 }
